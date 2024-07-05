5d1a917297d701d3001383f91eda5dbe905f7597
Russell Luther <mrluther@live.com>
2024-07-05 18:51:19 -0400

    change logrotate script to run every .conf file file in dir

--- logrotate/manage_rotate.sh
+++ logrotate/manage_rotate.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-# Rotate WindowsComputer log file
-logrotate /mnt/RussNAS/scripts/logrotate/rotate_WindowsComputer.conf
+# Determine the script's directory
+SCRIPT_DIR=$(dirname "$0")
 
-# Rotate plex-media log file
-logrotate /mnt/RussNAS/scripts/logrotate/rotate_plex-media.conf
+# Find all .conf files in SCRIPT_DIR
+find "$SCRIPT_DIR" -name '*.conf' -type f | while IFS= read -r file; do
+    # Get the current owner and group of the file
+    ORIGINAL_OWNER=$(stat -c "%U:%G" "$file")
 
-# Rotate organize_dji_files log file
-logrotate /mnt/RussNAS/scripts/logrotate/rotate_organize_dji_files.conf
+    # Change the owner to root
+    chown root:root "$file"
+    chmod 644 "$file"
+    # Run logrotate on the file
+    logrotate "$file"
+
+    # Set the owner back to the original owner
+    chown "$ORIGINAL_OWNER" "$file"
+    chmod 755 "$file"
+done
