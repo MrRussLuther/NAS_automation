6929689f09d293513a5910e4a422cd91d0a6f88c
Russell Luther <mrluther@live.com>
2024-07-05 19:01:26 -0400

    add chmod + reduce rotate size

--- logrotate/manage_rotate.sh
+++ logrotate/manage_rotate.sh
@@ -5,16 +5,18 @@ SCRIPT_DIR=$(dirname "$0")
 
 # Find all .conf files in SCRIPT_DIR
 find "$SCRIPT_DIR" -name '*.conf' -type f | while IFS= read -r file; do
-    # Get the current owner and group of the file
+    # Get the current owner, group, and permissions of the file
     ORIGINAL_OWNER=$(stat -c "%U:%G" "$file")
+    ORIGINAL_PERMISSIONS=$(stat -c "%a" "$file")
 
     # Change the owner to root
     chown root:root "$file"
-    chmod 644 "$file"
+    chmod 600 "$file"
+
     # Run logrotate on the file
     logrotate "$file"
 
-    # Set the owner back to the original owner
+    # Set the owner and permissions back to the original values
     chown "$ORIGINAL_OWNER" "$file"
-    chmod 755 "$file"
+    chmod "$ORIGINAL_PERMISSIONS" "$file"
 done
