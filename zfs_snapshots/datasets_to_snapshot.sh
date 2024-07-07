a72b37940583d7b2a5d30d2d41e2d0c67923d5f0
Russell Luther <mrluther@live.com>
2024-07-07 11:38:20 -0400

    all of this was pointless...

--- zfs_snapshots/datasets_to_snapshot.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Sanpshot WindowsComputer dataset
-bash /mnt/RussNAS/scripts/zfs_snapshots/manage_snapshots.sh RussNAS/WindowsComputer
-
-# Sanpshot plex-media dataset
-bash /mnt/RussNAS/scripts/zfs_snapshots/manage_snapshots.sh RussNAS/plex/plex-media
